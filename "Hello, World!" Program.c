48bcfb0a05a66383c887cc498d8d0cf65e9f7ada
rathesh <ratheshprabakar@gmail.com>
2019-10-02 22:08:14 +0530

    First commit Program to print Hello world, Program to add two numbers

---
+++ "Hello, World!" Program.c
@@ -0,0 +1,6 @@
+//1.To print Hello world
+#include<stdio.h>
+void main()
+{
+  printf("Hello world");
+}
---
+++ Program to Add Two Integers.c
@@ -0,0 +1,13 @@
+//C program to add two integers
+#include<stdio.h>
+void main()
+{ 
+  int a,b;
+  printf("Enter two numbers\t");
+  //Get the input for two integers
+  scanf("%d %d",&a,&b);
+  //printing sum of two numbers
+  printf("The sum of two numbers are \t%d",a+b);
+  
+}
+
