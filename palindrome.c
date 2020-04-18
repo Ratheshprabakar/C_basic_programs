a13529b57eee4f52ce29a044dece0cc2c563adde
Rathesh <ratheshprabakar@gmail.com>
2020-04-18 23:11:57 +0530

    Updated palindrome.c without string functions

--- palindrome.c
+++ palindrome.c
@@ -1,15 +1,23 @@
+/*C program to check whether the given string is palindrome*/
 #include<stdio.h>
-void main()
+int main()
 {
-	char a[20],b[20];
-	printf("Enter a string");
-	scanf("%s",a);
-	strcpy(b,a);
-	strrev(b);
-	if(strcmp(a,b)==0)
+	char input_string[100];
+	int i=0,len,flag=0;
+	printf("Enter the string\t");
+	gets(input_string);
+	len=strlen(input_string)-1;
+	while(len>i)
 	{
-		printf("The string is PALINDROME");
+		if(input_string[i++]!=input_string[len--])
+		{
+			printf("%s is not palindrome",input_string);
+			flag=0;
+			break;	
+		}
+		else{	
+		flag=1;}
 	}
-	else
-	 printf("The entered string is not a PALINDROME");
+	if(flag==1)	
+		printf("%s is palindrome",input_string);
 }
